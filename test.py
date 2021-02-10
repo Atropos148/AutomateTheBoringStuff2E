d33e40bcf607d91e1e7a35108e01d65884500217
atropos148 <tibor.h148@gmail.com>
2021-02-10 12:39:45 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+import webbrowser
+import sys
+
+
+def main():
+    print("Hello")
+    # webbrowser.open("https://inventwithpython.com/")
+    print(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
