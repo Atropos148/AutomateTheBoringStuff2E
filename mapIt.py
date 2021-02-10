b6e46fbba5106e4c96c90d1a5fa84c18368bfc0d
atropos148 <tibor.h148@gmail.com>
2021-02-10 14:19:32 +0100

    Add 2 functions to separate code

--- mapIt.py
+++ mapIt.py
@@ -1,10 +1,24 @@
 import webbrowser
 import sys
 import pyperclip as pc
+import tkinter as tk
 
 
 def main():
     print("Open Google Maps location")
+    open_map()
+
+    cities = ["New York", "Paris", "Prague", "Seoul"]
+    for city in cities:
+        open_map_specific(city)
+
+
+def open_map_specific(location: str):
+    webbrowser.open(
+        "https://www.google.com/maps/place/" + location)
+
+
+def open_map():
     if len(sys.argv) == 2:
         webbrowser.open(
             "https://www.google.com/maps/place/" + sys.argv[1])
