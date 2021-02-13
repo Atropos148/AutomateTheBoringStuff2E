d7bcc30d003d5c5bbb9a61e86603573b6186d04c
atropos148 <tibor.h148@gmail.com>
2021-02-13 18:15:08 +0100

    Refacor for stackedit

--- CV_writer.py
+++ CV_writer.py
@@ -6,18 +6,21 @@ import time
 
 def main():
     browser = webdriver.Edge('msedgedriver.exe')
-    browser.get('https://write.as/')
+    browser.get('https://stackedit.io/')
+    browser.implicitly_wait(10)
 
     try:
         time.sleep(2)
-        im_not_a_robot_button = browser.find_element_by_css_selector(
-            'a.btn.cta')
-        im_not_a_robot_button.click()
-        time.sleep(5)
-        dark_theme_button = browser.find_element_by_id('toggle-theme')
-        dark_theme_button.click()
+        write_button = browser.find_element_by_css_selector(
+            'a.navigation-bar__button')
+        write_button.click()
 
-        time.sleep(100)
+        time.sleep(4)
+        skip_tutorial_button = browser.find_element_by_css_selector(
+            ".tour-step__button-bar")
+        print(skip_tutorial_button)
+
+        time.sleep(10000000)
 
     except Exception:
         print("I didn't find an element with that name")
